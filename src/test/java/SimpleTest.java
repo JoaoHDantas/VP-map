6b6e4d2a6e8f9d158c5b9e44b2a4390d1fff9c9b
Luan Scolfaro Amorim Carneiro <scolfaroluan@gmail.com>
2024-12-06 11:27:04 -0300

    testes

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+class SimpleTest {
+    @Test
+    void testExample() {
+        assertTrue(true);
+    }
+}
